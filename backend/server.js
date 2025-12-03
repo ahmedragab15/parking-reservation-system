9e6631a32af599756f679708db6863fcc9ef3b9e
ahmedragab15 <ragabahmed154@gmail.com>
2025-12-03 17:30:41 +0200

    update cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ const PORT = process.env.PORT || 3000;
 const BASE = '/api/v1';
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONT_END_URL || 'http://localhost:3001'],
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 
 const seed = JSON.parse(fs.readFileSync(path.join(__dirname, 'seed.json')));
